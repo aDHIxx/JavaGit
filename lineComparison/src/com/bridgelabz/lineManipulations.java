3ccffdf5a3cb30451e5e14f340705d0a5bc6fdb6
aDHIxx <sreemohan.adhithya@gmail.com>
2023-11-23 01:38:39 +0530

    Added: UseCase 2: Check if lines are equal

--- lineComparison/src/com/bridgelabz/lineManipulations.java
+++ lineComparison/src/com/bridgelabz/lineManipulations.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class lineManipulations {
+
     private double x1, y1, x2, y2;
 
     public lineManipulations(double x1, double y1, double x2, double y2) {
@@ -13,28 +14,42 @@ public class lineManipulations {
     }
 
     /**
-     * @name: calculateLength
-     * @return/ o/p: length of line using the distance formula.
+     * @name: equals
+     * @return/ o/p: true if the lines are equal, false otherwise
      */
-    public double calculateLength() {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+    public boolean equals(lineManipulations other) {
+        return this.x1 == other.x1 && this.y1 == other.y1 && this.x2 == other.x2 && this.y2 == other.y2;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("x1 y1 : ");
-        double x1 = scanner.nextDouble();
-        double y1 = scanner.nextDouble();
+        System.out.print("Line 1: x1  y1: ");
+        double x1Line1 = scanner.nextDouble();
+        double y1Line1 = scanner.nextDouble();
 
-        System.out.print("x2 y2 : ");
-        double x2 = scanner.nextDouble();
-        double y2 = scanner.nextDouble();
+        System.out.print("Line 1: x2  y2: ");
+        double x2Line1 = scanner.nextDouble();
+        double y2Line1 = scanner.nextDouble();
 
-        lineManipulations line = new lineManipulations(x1, y1, x2, y2);
+        lineManipulations line1 = new lineManipulations(x1Line1, y1Line1, x2Line1, y2Line1);
 
-        double length = line.calculateLength();
-        System.out.println("Length of the line: " + length);
+        System.out.print("Line 2: x1  y1: ");
+        double x1Line2 = scanner.nextDouble();
+        double y1Line2 = scanner.nextDouble();
+
+        System.out.print("Line 2: x2  y2: ");
+        double x2Line2 = scanner.nextDouble();
+        double y2Line2 = scanner.nextDouble();
+
+        lineManipulations line2 = new lineManipulations(x1Line2, y1Line2, x2Line2, y2Line2);
+
+        if (line1.equals(line2)) {
+            System.out.println("Lines are equal.");
+        } else {
+            System.out.println("Lines are not equal.");
+        }
 
         scanner.close();
     }
