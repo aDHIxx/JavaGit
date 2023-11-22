72493f9b9c5dd6c30d933a775f321cbeee106173
aDHIxx <sreemohan.adhithya@gmail.com>
2023-11-23 00:09:34 +0530

    Added: UseCase 6: Calc monthly wage until the meximium condition

--- employeeWageGit/src/com/bridgelabz/calcParttimeOrFullTimeWage.java
+++ employeeWageGit/src/com/bridgelabz/calcParttimeOrFullTimeWage.java
@@ -6,7 +6,8 @@ public class calcParttimeOrFullTimeWage {
     private static final int HOURLY_WAGE = 20;
     private static final int FULL_TIME_HOURS = 8;
     private static final int PART_TIME_HOURS = 4;
-    private static final int WORKING_DAYS_IN_MONTH = 20;
+    private static final int MAX_WORKING_HOURS = 100;
+    private static final int MAX_WORKING_DAYS = 20;
 
     /**
      * @name: calculateDailyWage
@@ -27,27 +28,35 @@ public class calcParttimeOrFullTimeWage {
 
     public static void main(String[] args) {
         double totalWage = 0;
+        int totalWorkingHours = 0;
+        int totalWorkingDays = 0;
 
-        for (int day = 1; day <= WORKING_DAYS_IN_MONTH; day++) {
+        while (totalWorkingHours < MAX_WORKING_HOURS && totalWorkingDays < MAX_WORKING_DAYS) {
             int attendanceStatus = generateAttendance();
             int hoursWorked = 0;
 
             switch (attendanceStatus) {
                 case FULL_TIME:
-                    System.out.println("Day " + day + ": Employee is Present - Full Time");
+                    System.out.println("Day " + (totalWorkingDays + 1) + ": Employee is Present - Full Time");
                     hoursWorked = FULL_TIME_HOURS;
                     break;
                 case PART_TIME:
-                    System.out.println("Day " + day + ": Employee is Present - Part Time");
+                    System.out.println("Day " + (totalWorkingDays + 1) + ": Employee is Present - Part Time");
                     hoursWorked = PART_TIME_HOURS;
                     break;
                 default:
-                    System.out.println("Day " + day + ": Employee is Absent");
+                    System.out.println("Day " + (totalWorkingDays + 1) + ": Employee is Absent");
             }
 
             totalWage += calculateDailyWage(hoursWorked);
+            totalWorkingHours += hoursWorked;
+            totalWorkingDays++;
         }
-        System.out.println("Total Monthly Employee Wage: $" + totalWage);
+
+        // Display the total monthly wage, working hours, and working days
+        System.out.println("Total Monthly Employee Wage: " + totalWage);
+        System.out.println("Total Working Hours: " + totalWorkingHours);
+        System.out.println("Total Working Days: " + totalWorkingDays);
     }
 
 }
